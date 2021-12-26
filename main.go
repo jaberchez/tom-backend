3eaad52ddf91387ef29ed03de5f35077af81bf48
jaberchez <josan5@yahoo.es>
2021-12-27 00:48:14 +0100

    Add handler for SIGINT and SIGTERM signals

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -67,6 +70,34 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			// Unexpected error, port in use?
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
 	log.Printf("Server listening or port %s\n", listenPort)
-	log.Fatal(srv.ListenAndServe())
+
+	stopC := make(chan os.Signal)
+	signal.Notify(stopC, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-stopC
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	switch sig {
+	case syscall.SIGTERM:
+		log.Println("got signal SIGTERM")
+	case syscall.SIGINT:
+		log.Println("got signal SIGINT")
+	default:
+		log.Println("got unknown signal")
+	}
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err) // Failure/timeout shutting down the server gracefully
+	}
+
+	log.Println("server exited properly")
 }
